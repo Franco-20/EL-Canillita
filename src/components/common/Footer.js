b5f90ac3d92dc2061d07cda01e4248a4d07cd51a
unknown <galarcediego@gmail.com>
2020-09-11 00:04:32 -0300

    se crearon los iconos de las redes sociales

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faLinkedin, faTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram, faLinkedin, faTwitter, faWhatsapp, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
@@ -17,7 +17,7 @@ const Footer = () => {
                         <img className="w-25" src="img/canillitaPrueba.png" alt="" />
                         <p>25 de Mayo 654, S.M. Tucumán
                     Tucumán (T4000DAN)</p>
-                    <h4>Contacto</h4>
+                        <h4>Contacto</h4>
                         <Button variant="light" href="#" className="m-1"><FontAwesomeIcon style={{ fontSize: "3em" }} icon={faWhatsapp} /></Button>
                         <Button variant="light" href="#" className="m-1"><FontAwesomeIcon style={{ fontSize: "3em" }} icon={faEnvelope} /></Button>
                     </div>
@@ -27,6 +27,7 @@ const Footer = () => {
                         <Button variant="light" href="#" className="m-1"><FontAwesomeIcon style={{ fontSize: "3em" }} icon={faFacebook} /></Button>
                         <Button variant="light" href="#" className="m-1"><FontAwesomeIcon style={{ fontSize: "3em" }} icon={faLinkedin} /></Button>
                         <Button variant="light" href="#" className="m-1"><FontAwesomeIcon style={{ fontSize: "3em" }} icon={faTwitter} /></Button>
+                        <Button variant="light" href="#" className="m-1"><FontAwesomeIcon style={{ fontSize: "3em" }} icon={faYoutube} /></Button>
                     </div>
                 </div>
             </div>
