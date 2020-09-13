cce1320a3104f8f05ff4da5e5f890bc919a240b0
esteban <estebanvan9@gmail.com>
2020-09-13 20:06:54 -0300

    Creación del componente Api-moneda-clima

---
+++ src/components/principal/ApiClimaMoneda.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApiClimaMoneda = () => {
+    return(
+        <>
+        <h1>ApiClimaMoneda</h1>
+     </>
+    )
+}
+
+export default ApiClimaMoneda;
--- src/components/principal/Inicio.js
+++ src/components/principal/Inicio.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ApiClimaMoneda from './ApiClimaMoneda';
 
 const Inicio = () => {
     return (
         <div>
+            <ApiClimaMoneda/>
             <h1>Inici4444o</h1>
             <h1>Inicio</h1>
             <h1>Inicio</h1>
