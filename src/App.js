4d5bfae10e1e7bfba733ae2af325c4548bda6bac
esteban <estebanvan9@gmail.com>
2020-09-14 15:34:06 -0300

    creé un button (Link) en la pagina de error 404 que los redireccionará a la pagina de inicio

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         <Route exact path="/tecnologia">
           <Tecnologia></Tecnologia>
         </Route>
-        <Route exact path="/error404">
+        <Route exact path="*">
           <Error404></Error404>
         </Route>
       </Switch>
--- src/components/error/Error404.js
+++ src/components/error/Error404.js
@@ -1,9 +1,14 @@
 import React from 'react';
+// import Img from './imgError/error404.png';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
-        <div>
-            <h1>gola</h1>
+        <div className='text-center'>
+            {/* <img src={Img} alt='1'  className=' w-100'></img> */}
+           <Link to='/' className='btn btn-primary  my-3'>
+               Volver a Inicio?
+          </Link>
         </div>
     );
 };
