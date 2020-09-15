c977c1e66282e27f073ccbd2c9465ef2ace3b317
Franco Olmi <olmi9899@gmail.com>
2020-09-15 01:52:19 -0300

    se agrego banner sobre cuidados sobre el covid

--- src/components/principal/Inicio.js
+++ src/components/principal/Inicio.js
@@ -1,28 +1,19 @@
-import React from 'react';
-import ApiClimaMoneda from './ApiClimaMoneda';
+import React from "react";
+import ApiClimaMoneda from "./ApiClimaMoneda";
+import Card from "react-bootstrap/Card";
 
 const Inicio = () => {
-    return (
-        <div>
-            <ApiClimaMoneda/>
-            <h1>Inici4444o</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-            <h1>Inicio</h1>
-        </div>
-    );
+  return (
+    <div>
+      <ApiClimaMoneda />
+      <div className="container">
+          <article className="text-center">
+            <img src={process.env.PUBLIC_URL + "virus.gif"} alt=""/>
+          </article>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Inicio;
