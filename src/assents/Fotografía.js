060929e586ca8c93270dbba1564db051633f3077
unknown <galarcediego@gmail.com>
2020-09-15 00:11:06 -0300

    se realizo una galeria en css sin trabajar en javascrip

--- src/assents/Fotografía.js
+++ src/assents/Fotografía.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Qemao from '../assents/img/quema.jpg';
+import ImagenUno from '../assents/img/quema.jpg';
+import ImagenDos from '../assents/img/manzur-gastona.jpg';
+import ImagenTres from '../assents/img/cloacas.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Fotografía = () => {
     return (
         <>
-        <Container>
-            <Row>
-                <Col lg={8} md={12}>
-                <div className="Container">
-                            <h1 className="my-4"><strong>Coronavirus en Tucumán: 9 muertes y 543 casos en total este sábado</strong></h1>
-                            <h3 className="my-4">PANDEMIA | Hace instantes el Ministerio de Salud dio su reporte vespertino del virus: a las cifras de la mañana se sumaron 3 fallecimientos y 183 positivos. Todos los números.</h3>
-                        </div>
-                        <h1>Imagenes</h1>
-                    <img alt="" src={Qemao} className="" />
-                </Col>
-            </Row>
-        </Container>
+            <h1 className="my-4 text-center"><strong>Postales de Tucumán</strong></h1>
+            <section className="galeria">
+                <a className="efecto" href="#quema.jpg"><img className="imagen" src={ImagenUno} alt="Quema de cañaveral" /></a>
+                <a className="efecto" href="#manzur-gastona.jpg"><img className="imagen" src={ImagenDos} alt="manzur" /></a>
+                <a className="efecto" href="#cloacas.jpg"><img className="imagen" src={ImagenTres} alt="cloacas" /></a>
+            </section>
+            
+            <article className="light-box" id="quema.jpg">
+                <a href="#cloacas.jpg" className="next"><FontAwesomeIcon icon={faArrowLeft} /></a>
+                <img src={ImagenUno} alt="" />
+                <a href="#manzur-gastona.jpg" className="next"><FontAwesomeIcon icon={faArrowRight} /></a>
+                <a href="#" className="close">x</a>
+            </article>
+
+            <article className="light-box" id="manzur-gastona.jpg">
+                <a href="#quema.jpg" className="next"><FontAwesomeIcon icon={faArrowLeft} /></a>
+                <img src={ImagenDos} alt="" />
+                <a href="#cloacas.jpg" className="next"><FontAwesomeIcon icon={faArrowRight} /></a>
+                <a href="#" className="close">x</a>
+            </article>
+
+            <article className="light-box" id="cloacas.jpg">
+                <a href="#manzur-gastona.jpg" className="next"><FontAwesomeIcon icon={faArrowLeft} /></a>
+                <img src={ImagenTres} alt="" />
+                <a href="#quema.jpg" className="next"><FontAwesomeIcon icon={faArrowRight} /></a>
+                <a href="#" className="close">x</a>
+            </article>
+
+            <article className="light-box" id="quema.jpg">
+                <a href="#cloacas.jpg" className="next"><FontAwesomeIcon icon={faArrowLeft} /></a>
+                <img src={ImagenTres} alt="" />
+                <a href="#manzur-gastona.jpg" className="next"><FontAwesomeIcon icon={faArrowRight} /></a>
+                <a href="#" className="close">x</a>
+            </article>
         </>
     );
 };
