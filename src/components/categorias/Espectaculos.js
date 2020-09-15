4cf570df6c635221eba26229bc71ac1b8507e7c8
Tomas Quiroga <quirogatomas30@gmail.com>
2020-09-15 00:58:40 -0300

    se creo el maquetado para las distintas secciones

--- src/components/categorias/Espectaculos.js
+++ src/components/categorias/Espectaculos.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 const Espectaculos = () => {
-    return (
-        <div>
-            <h1>Espectaculos</h1>
-        </div>
-    );
+  return (
+    <div className="container my-4">
+      <h1 className="text-center">
+        <strong>Espectaculos</strong>
+        <hr/>
+      </h1>
+      <Card>
+        <Card.Img
+          variant="top"
+          src="holder.js/100px180"
+          alt="imagen de la noticia"
+        />
+        <Card.Body>
+        <Card.Title>Titulo de la noticia.</Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Espectaculos;
--- src/components/categorias/Fotografía.js
+++ src/components/categorias/Fotografía.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
 
 const Fotografía = () => {
-    return (
-        <div>
-            <h1>Fotografia</h1>
-        </div>
-    );
+  return (
+    <div className="container my-4">
+      <h1 className="text-center">
+        <strong>Fotografia</strong>
+        <hr/>
+      </h1>
+      <CardDeck>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" alt="error,imagen mal cargada" />
+          <Card.Body>
+            <Card.Title>Titulo de la foto</Card.Title>
+            <Card.Text>
+              Redactar una pequeña reseña
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Hace 1 Hs</small>
+          </Card.Footer>
+        </Card>
+        
+      </CardDeck>
+    </div>
+  );
 };
 
 export default Fotografía;
