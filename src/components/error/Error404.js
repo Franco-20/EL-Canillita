dfde12b5b5807755ccd0d5bafa5d6c67a78357a9
Barbara <mahara14@hotmail.com>
2020-09-17 13:39:18 -0300

    corregi la ruta de error404

--- src/components/error/Error404.js
+++ src/components/error/Error404.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Img from './imgError/error404.png';
+import Img from './imgError/.png';
 import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
-        <div className='text-center error404'>
+        <div className='text-center '>
             <img src={Img} alt='1'  className=' w-100'></img>
              <h5>
                  Disculpa, Al parecer el artículo que buscas ya no este disponible,
