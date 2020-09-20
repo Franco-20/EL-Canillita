729dfbf44bae2ea51075fdbdd803a90c4bcc3017
Daiana Olmi <dai-olmi@hotmail.com>
2020-09-20 19:35:57 -0300

    actualice nuevamente los link de cada categoria con la respectiva ruta id

--- src/components/categorias/Economía.js
+++ src/components/categorias/Economía.js
@@ -44,7 +44,7 @@ const Economia = () => {
                    <p className="card-text"> {item.principal}</p>
                    </div>
                  </Card.Body>
-                 <Link to={`/noticia/${props.producto._id}`} className='btn btn-primary my-2 p-2'>
+                 <Link to={`/noticia/${item._id}`} className='btn btn-primary my-2 p-2'>
                        ver Más.!!
                    </Link>
                 </Card>
